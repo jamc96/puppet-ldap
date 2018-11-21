6e2112412c3498f41f58b808e91d426949377445
jamc96 <jamc.mejia@gmail.com>
2018-11-21 09:21:23 -0600

    removing provider authconfig

--- spec/unit/puppet/type/authconfig_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'puppet/type/authconfig'
-
-RSpec.describe 'the authconfig type' do
-  it 'loads' do
-    expect(Puppet::Type.type(:authconfig)).not_to be_nil
-  end
-end
