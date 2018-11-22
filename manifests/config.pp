6be27eda7b130aa42fa20a8d82c7e8d686e9dd2b
Jose Mejia <jamc.mejia@gmail.com>
2018-11-22 14:58:50 -0600

    removing not needed string parsing

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,7 +5,7 @@
 class ldap::config inherits ldap {
   # Making the configuration with authconfig
   exec { 'authconfig_ldap':
-    command     => "$::ldap::authconfig_ldap",
+    command     => $::ldap::authconfig_ldap,
     path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
     refreshonly => $::ldap_connection,
   }
